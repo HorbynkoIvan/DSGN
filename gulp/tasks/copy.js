52e76f3e71f55982582ec7b3800904311c98f00a
HorbynkoIvan <horbynko.i@gmail.com>
2020-09-12 14:39:11 +0300

    prepare project

---
+++ gulp/tasks/copy.js
@@ -0,0 +1,13 @@
+module.exports = function () {
+    $.gulp.task('copy', function () {
+        return $.gulp.src(
+            [
+                '/assets/fonts/**/*.{woff,woff2}',
+                '/assets/img/**',
+                '*.html'
+            ],
+            {base: '.'}
+        )
+            .pipe($.gulp.dest('build'))// create folder and put file there
+    })
+}
---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+global.$ = {
+    gulp: require('gulp'),
+    gp: require('gulp-load-plugins')(),
+    browserSync: require('browser-sync').create(),
+    del: require('del'),
+    imageminJpegRecompress: require('imagemin-jpeg-recompress'),
+    pngquant: require('imagemin-pngquant'),
+    path: {
+        config: require('./gulp/config'),
+        jquery: './js/jquery.js',
+        js: './js/**/*.js',
+    }
+};
+
+//use loop for connecting all tasks
+$.path.config.forEach(function(path) {
+    require(path)()
+})
+
